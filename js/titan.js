18d31515ff68fecd483c621b3f940766c6b40796
NGUYEN TIEN THANH <traceformula@gmail.com>
2015-02-23 17:34:37 -0500

    change font-family, add footer

--- js/titan.js
+++ js/titan.js
@@ -2,4 +2,8 @@ $(document).ready(function(){
     $('.carousel').carousel({
         interval: 5000 //changes the speed
     });
+    /* smooth scrolling for scroll to top */
+    $('.scroll-top').click(function(){
+      $('body,html').animate({scrollTop:0},1000);
+    });
 });
