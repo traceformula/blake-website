bf82cd10943ff2239a7400e5bbe32a783cbd2ec9
NGUYEN TIEN THANH <traceformula@gmail.com>
2015-03-08 00:27:57 -0500

    add signin & sign up modal dialog

--- js/titan.js
+++ js/titan.js
@@ -7,3 +7,16 @@ $(document).ready(function(){
       $('body,html').animate({scrollTop:0},1000);
     });
 });
+
+/*function centerModal() {
+    $(this).css('display', 'block');
+    var $dialog = $(this).find(".modal-dialog");
+    var offset = ($(window).height() - $dialog.height()) / 2;
+    // Center modal vertically in window
+    $dialog.css("margin-top", offset);
+}
+
+$('.modal').on('show.bs.modal', centerModal);
+$(window).on("resize", function () {
+    $('.modal:visible').each(centerModal);
+});*/
